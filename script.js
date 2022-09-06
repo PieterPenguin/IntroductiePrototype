44b03c45a5bd69f8b7113e7f6bcf366d4a40fe06
CornellVanDerStraaten <cornellbvanders@gmail.com>
2022-09-06 09:02:15 +0200

    Refactored personal answers

--- script.js
+++ script.js
@@ -2,18 +2,37 @@ let quiz = {
     questionTypes: ['twoTruthOneLie', 'wordTooMany'],
     questions: [
         {
-            questionOne: 'Wat is het favoriete dier van',
-            typeOfQuestion: 'wordTooMany'
+            id: 1,
+            question: 'Wat is het favoriete dier van',
+            type: 'wordTooMany'
         }
     ],
     people: [
         {
+            id: 1,
+            name: 'Benno',
+            age: 51,
+            answers: [
+                {
+                    questionId: 1,
+                    answer: 'kat'
+                }
+            ],
+            personalQuizFinished: false
+        },
+        {
+            id: 2,
             name: 'Cornell',
             age: 19,
-            answerToOne: 'Kat',
+            answers: [
+                {
+                    questionId: 1,
+                    answer: 'beest'
+                }
+            ],
             personalQuizFinished: false
-        }
-    ]
+        },
+    ],
 }
 
 document.getElementById('button').addEventListener('click', startQuiz);
