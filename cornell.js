c1a883bd329e6974e0b99f07b57ea2fccd8bc5db
CornellVanDerStraaten <cornellbvanders@gmail.com>
2022-09-06 21:29:41 +0200

    Refactored variable names for readability

--- cornell.js
+++ cornell.js
@@ -7,21 +7,21 @@ let questions = [
         id: 1,
         question: 'Wat is het favoriete dier van Cornell?',
         answer: 'Kat',
-        possibleAnswers: ['Marmot', 'Beest', 'Mens', 'Kat'],
+        options: ['Marmot', 'Beest', 'Mens', 'Kat'],
         answered: false
     }, {
         id: 2,
         question: 'Wat is het favoriete eten van Cornell?',
         answer: 'Vlees',
-        possibleAnswers: ['Vlees', 'Groenten', 'Blegh', 'Dier'],
+        options: ['Vlees', 'Groenten', 'Blegh', 'Dier'],
         answered: false
     }
 ];
 
-let answeredQuestions = [];
+
 
 /** HTML elements **/
-let question = document.getElementById('question');
+let questionElement = document.getElementById('question');
 let answerFields = [
     document.getElementById('answerOne'),
     document.getElementById('answerTwo'),
@@ -33,25 +33,46 @@ function startQuiz() {
     initializeQuestion();
 }
 
+let answeredQuestions = [];
+let currentQuestion = null;
+let currentQuestionCorrectButton = null;
 function initializeQuestion() {
-    let currentQuestion = questions[Math.floor(Math.random() * questions.length)];
+    let question = questions[Math.floor(Math.random() * questions.length)];
 
-    if (currentQuestion.answered) {
+    if (question.answered) {
         initializeQuestion()
     }
 
-    setQuestion(currentQuestion);
+    currentQuestion = question;
+    setQuestion();
 }
 
-function setQuestion(currentQuestion) {
-    question.innerText = currentQuestion.question;
+/**
+ * Fill buttons and title with correct text
+ */
+function setQuestion() {
+    questionElement.innerText = currentQuestion.question;
 
-    for ( const [i, value] of answerFields.entries()) {
-        value.innerText = currentQuestion.possibleAnswers[i];
+    for ( const [i, element] of answerFields.entries()) {
+        let currentOption = currentQuestion.options[i];
+
+        // Set the correct answered button to global variable
+        if (currentOption === currentQuestion.answer) {
+            currentQuestionCorrectButton = element;
+        }
+
+        element.innerText = currentOption;
     }
 }
 
-function checkAnswer(answer) {
-    console.log(answer);
+function checkAnswer(clickedAnswerElement) {
+    if (clickedAnswerElement.innerText === currentQuestion.answer) {
+        // Set clicked box to green
+    } else {
+        // Set clicked box to red
+        // Correct answer to green
+    }
+
+    // Show button to next question
 }
 
